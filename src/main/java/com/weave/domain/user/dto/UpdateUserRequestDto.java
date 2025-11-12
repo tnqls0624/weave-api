7d516f8e76884efe0bf0677ebc4211a397352733
이수빈 <dktnqls0624@naver.com>
2025-11-12 23:32:37 +0900

    fix: update user dto

--- src/main/java/com/weave/domain/user/dto/UpdateUserRequestDto.java
+++ src/main/java/com/weave/domain/user/dto/UpdateUserRequestDto.java
@@ -15,6 +15,8 @@ public class UpdateUserRequestDto {
   @Size(max = 10, message = "이름은 최대 10자까지 입력 가능합니다.")
   private String name;
 
+  private String birthday;
+
   private String fcmToken;
   private Boolean pushEnabled;
   private String avatarUrl;
