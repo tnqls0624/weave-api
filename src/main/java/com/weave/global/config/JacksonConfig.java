7ec5f8a72a949b2702bee1cfb6fb0a4fa2c064e5
이수빈 <dktnqls0624@naver.com>
2025-11-13 16:51:15 +0900

    fix: object syrialize

--- src/main/java/com/weave/global/config/JacksonConfig.java
+++ src/main/java/com/weave/global/config/JacksonConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.IOException;
 import org.bson.types.ObjectId;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -17,6 +18,7 @@ public class JacksonConfig {
   @Bean
   public Jackson2ObjectMapperBuilderCustomizer objectIdCustomizer() {
     return builder -> {
+      // ObjectId를 String으로 직렬화
       builder.serializerByType(
           org.bson.types.ObjectId.class,
           com.fasterxml.jackson.databind.ser.std.ToStringSerializer.instance
@@ -34,7 +36,12 @@ public class JacksonConfig {
             }
           }
       );
+
+      // Java 8 date/time 모듈 자동 등록
       builder.findModulesViaServiceLoader(true);
+
+      // Date를 timestamp가 아닌 ISO-8601 문자열로 직렬화
+      builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     };
   }
 
