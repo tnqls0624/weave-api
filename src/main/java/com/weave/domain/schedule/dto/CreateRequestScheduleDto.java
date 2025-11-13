e157705573e85668156f2262a0cf44d0160d0b02
이수빈 <dktnqls0624@naver.com>
2025-11-14 01:00:14 +0900

    fix: create schedule dto

--- src/main/java/com/weave/domain/schedule/dto/CreateRequestScheduleDto.java
+++ src/main/java/com/weave/domain/schedule/dto/CreateRequestScheduleDto.java
@@ -2,7 +2,6 @@ package com.weave.domain.schedule.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.Date;
@@ -19,7 +18,7 @@ public class CreateRequestScheduleDto {
   private String workspace;
 
   @Schema(example = "누나생일", description = "제목")
-  @NotBlank
+  @NotNull
   private String title;
 
   @Schema(example = "누나 생일은 10월 6일", description = "설명")
@@ -35,8 +34,8 @@ public class CreateRequestScheduleDto {
   @NotNull
   private Date endDate;
 
+  //default: none
   @Schema(example = "none", description = "반복 타입")
-  @NotNull
   private String repeatType;
 
   @Schema(example = "[\"66a61517670be7ef30b10244\", \"66a7ae7f25483684cf347cd9\"]", description = "참여자")
@@ -44,6 +43,5 @@ public class CreateRequestScheduleDto {
   private List<String> participants;
 
   @Schema(example = "solar", description = "양력, 음력")
-  @NotNull
   private String calendarType;
 }
