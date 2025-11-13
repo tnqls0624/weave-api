e496261a2fedca5bbb7da9198c1b4334af027d6e
이수빈 <dktnqls0624@naver.com>
2025-11-13 17:07:35 +0900

    fix: ObjectMapper fix

--- src/main/java/com/weave/global/config/JacksonConfig.java
+++ src/main/java/com/weave/global/config/JacksonConfig.java
@@ -3,14 +3,12 @@ package com.weave.global.config;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.IOException;
 import org.bson.types.ObjectId;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfig {
@@ -37,10 +35,6 @@ public class JacksonConfig {
           }
       );
 
-      // Java 8 date/time 모듈 자동 등록
-      builder.findModulesViaServiceLoader(true);
-
-      // Date를 timestamp가 아닌 ISO-8601 문자열로 직렬화
       builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     };
   }
